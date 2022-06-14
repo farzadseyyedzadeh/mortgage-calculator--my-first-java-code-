3bd759718ef1b0d248896416a619324158b2a66b
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-06-14 22:51:44 +0430

    mortgage calculator v1.0

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Principal
+        System.out.print("Principal:");
+        int principal = scanner.nextInt();
+        // Interest rate
+        System.out.print("Annual Interest(percent): ");
+        double annualInterest = scanner.nextDouble();
+        double monthlyInterest = annualInterest / 12 / 100;
+        // Period
+        System.out.print("Period (Years): ");
+        int years = scanner.nextInt();
+        int numberOfPayments = years * 12;
+
+        scanner.close();
+        // Calculate the monthly payment
+        double mathPower = Math.pow(1 + monthlyInterest, numberOfPayments);
+        double monthlyPayment = principal * (monthlyInterest * mathPower / (mathPower - 1));
+
+        String monthlyPaymentFormatted = NumberFormat.getCurrencyInstance().format(monthlyPayment);
+        // Print the result
+        System.out.print(monthlyPaymentFormatted);
+    }
+}
